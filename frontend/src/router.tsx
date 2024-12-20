37ec86353d0c9f6029fdf853a410e258170f6533
tukkedd <jeamki2501@gmail.com>
2024-12-19 23:58:50 -0400

    layouts

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,15 +1,17 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import LoginView from './views/LoginView'
 import RegisterView from './views/RegisterView'
+import AuthLayout from './layouts/AuthLayout'
 
 export default function Router () {
     return (
         <BrowserRouter>
             <Routes>
                 <Route>
-                    <Route path='/auth/login' element={<LoginView />} />
-                    <Route path='/auth/register' element={<RegisterView />} />
-                    
+                    <Route element={<AuthLayout />} >
+                        <Route path='/auth/login' element={<LoginView />} />
+                        <Route path='/auth/register' element={<RegisterView />} />
+                    </Route>
                 </Route>
             </Routes>
         </BrowserRouter>
--- frontend/src/views/LoginView.tsx
+++ frontend/src/views/LoginView.tsx
@@ -1,6 +1,14 @@
-
+import { Link } from "react-router-dom"
 export default function LoginView() {
   return (
-    <div className="text-6xl">LoginView</div>
+    <>
+
+
+        <nav>
+            <Link to="/auth/register">
+                No tienes una cuenta?, Crea una aqui
+            </Link>
+        </nav>
+    </>
   )
 }
