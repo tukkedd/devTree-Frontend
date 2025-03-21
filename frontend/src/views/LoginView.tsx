5fe600d0ad0b3ddc7ddb1d4753c004705f650eb2
Tukkedd <jeamki2501@gmail.com>
2025-03-21 17:27:13 -0400

    navigation tabs, profile view and linktree view created and added to router

--- frontend/src/views/LoginView.tsx
+++ frontend/src/views/LoginView.tsx
@@ -17,8 +17,7 @@ export default function LoginView() {
   const handleLogin = async (formData: LoginForm) => {
     try {
       const {data} = await api.post(`/auth/login`, formData)
-      toast.success(data)
-
+      localStorage.setItem('AUTH_TOKEN', data)
 
     } catch (error) {
         if(isAxiosError(error) && error.response ) {
